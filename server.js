adb09cd8d3793ad23110ed085d1cfa7d9262b818
Lucas Felinto Pinto <lucasfepinto@gmail.com>
2020-04-14 11:34:37 -0300

    feat njk files
    configurate template enginer
    structurate dinamics pages

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
+// Requiring express and nunjucks
 const express = require('express');
-const server = express()
+const nunjucks = require('nunjucks')
+// server const by express
+const server = express()    
 
-server.get("/", function (req, res) {
-    return res.send("Hello, World!")
+// configurate to get styles, images and scripts
+server.use(express.static('public'))
+
+// setting the server - template egineer
+server.set("view engine", "njk")
+// configurates of nunjucks
+nunjucks.configure("views", {
+    express: server
 })
 
+//server routes
+server.get("/", function (req, res) {
+    return res.render("index")
+})
+
+server.get("/about", function (req, res) {
+    return res.render("about")
+})
+
+server.get("/receipes", function (req, res) {
+    return res.render("receipes")
+})
+
+
+// server host
 server.listen(5000, function(){
     console.log("server is running")
 })
