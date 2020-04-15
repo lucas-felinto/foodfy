67e384f46e481919b769db9a15c9ab096de0a139
Lucas Felinto Pinto <lucasfepinto@gmail.com>
2020-04-15 14:42:41 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-// Requiring express and nunjucks
 const express = require('express');
 const nunjucks = require('nunjucks')
-// server const by express
+
 const recipes = require('./data')
 const server = express()
 
-// configurate to get styles, images and scripts
 server.use(express.static('public'))
 
 // setting the server - template egineer
 server.set("view engine", "njk")
-// configurates of nunjucks
+
+// nunjucks configurate
 nunjucks.configure("views", {
     express: server,
     noCache: true
@@ -25,20 +24,16 @@ server.get("/about", function (req, res) {
     return res.render("about")
 })
 
- 
-
 server.get("/receipes", function (req, res) {
     return res.render("receipes", { recipes })
 })
-
 
 server.get("/recipes/:index", function (req, res){
     const index = req.params.index
     return res.render("recipes", {items: recipes, index})
 })
 
-
-
+// server local
 server.listen(5000, function(){
     console.log("server is running")
 })
