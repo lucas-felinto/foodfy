9501d8e9432b688da11a26a52ff8cdcf9ac4222f
lucasfelinto <lucasfepinto@gmai.com>
2020-05-08 17:21:01 -0300

    refactored controllers

--- src/app/controllers/recipes.js
+++ src/app/controllers/recipes.js
@@ -1,74 +1,66 @@
 const fs = require('fs')
 const data = require('../../../data.json')
 
-exports.index = function (req, res){
-    const index = req.params.index
-    return res.render("admin/recipes", {recipes: data.recipes, index})
-}
+module.exports = {
+    index (req, res) {
+        const index = req.params.index
+        return res.render("admin/recipes", {recipes: data.recipes, index})
+    },
+    create (req, res) {
+        return res.render("admin/create")
+    },
+    show (req, res) {
+        const index = req.params.index
+        const foundRecipe = data.recipes[index]
 
-exports.create = function (req, res){
-    return res.render("admin/create")
-}
+        if (!foundRecipe) return res.send('recipe not found')
 
-exports.show = function (req, res){
-    const index = req.params.index
-    const foundRecipe = data.recipes[index]
+        return res.render("admin/recipe", {items: data.recipes, index})
+    },
+    edit (req, res) {
+        const index = req.params.index
 
-    if (!foundRecipe) return res.send('recipe not found')
+        return res.render("admin/edit", {recipe: data.recipes, index})
+    },
+    post (req, res) {
+        data.recipes.push(req.body)
 
-    return res.render("admin/recipe", {items: data.recipes, index})
-}
+        fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+            if (err) return res.send("Erro")
+            
+            return res.redirect("/admin/recipes")
+        })
+    }, put (req, res) {
+        const index = req.params.index
+        const foundRecipe = data.recipes[index]
 
-exports.edit = function (req, res){
-    const index = req.params.index
+        if (!foundRecipe) return res.send('recipe not found')
 
-    return res.render("admin/edit", {recipe: data.recipes, index})
-}
+        const recipe = {
+            ...foundRecipe,
+            ...req.body
+        }
 
-exports.post = function (req, res){
+        data.recipes[index] = recipe
 
-    data.recipes.push(req.body)
+        fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+            if (err) return res.send("Write Files Error")
 
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
-        if (err) return res.send("Erro")
+            return res.redirect(`/admin/recipes`)
+        })  
+    }, delete (req, res) {
+        const index = req.params.index
     
-        return res.redirect("/admin/recipes")
-    })
-
-}
-
-exports.put = function (req, res){
-    const index = req.params.index
-    const foundRecipe = data.recipes[index]
-
-    if (!foundRecipe) return res.send('recipe not found')
-
-    const recipe = {
-        ...foundRecipe,
-        ...req.body
-    }
-
-    data.recipes[index] = recipe
-
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-        if (err) return res.send("Write Files Error")
-
-        return res.redirect(`/admin/recipes`)
-})
-}
-
-exports.delete = function (req, res){
-    const index = req.params.index
-    
-    function removeRecipe () {
+        function removeRecipe () {
         data.recipes.splice(index, 1)
+        }
+
+        removeRecipe()
+
+        fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+            if (err) return res.send("Write Files Error")
+
+            return res.redirect(`/admin/recipes`)
+        })
     }
-
-    removeRecipe()
-
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-        if (err) return res.send("Write Files Error")
-
-        return res.redirect(`/admin/recipes`)
-})
 }
--- src/app/controllers/site.js
+++ src/app/controllers/site.js
@@ -1,14 +1,14 @@
 const data = require('../../../data.json')
 
-exports.about = function (req, res) {
-    return res.render("site/about")
-}
-
-exports.recipes = function (req, res) {
-    return res.render("site/recipes", { recipes: data.recipes })
-}
-
-exports.recipe = function (req, res){
-    const index = req.params.index
-    return res.render("site/recipe", {items: data.recipes, index})
+module.exports = {
+    about (req, res) {
+        return res.render("site/about")
+    },
+    recipes (req, res) {
+        return res.render("site/recipes", { recipes: data.recipes })
+    },
+    recipe (req, res) {
+        const index = req.params.index
+        return res.render("site/recipe", {items: data.recipes, index})
+    }
 }
