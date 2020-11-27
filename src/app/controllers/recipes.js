c0484b16c745aa9da297836a641f850a0cdfd99c
lucas-felinto <lucasfepinto@gmail.com>
2020-11-26 21:59:42 -0300

    lint and refactory code

--- src/app/controllers/recipes.js
+++ src/app/controllers/recipes.js
@@ -1,41 +1,68 @@
-const fs = require('fs')
-const Recipe = require('../models/recipe')
+const RecipeModel = require('../models/recipe');
 
 module.exports = {
-    async index (req, res) {
-        let recipes = (await Recipe.recipeInformations()).rows
-        return res.render("admin/recipes/recipes", { recipes })
-    },
-    async create (req, res) {
-        let chefOptions = (await Recipe.chefSelectOptions()).rows
-        return res.render("admin/recipes/create", { chefOptions })
-    },
-    async show (req, res) {
-        const { id } = req.params
-        let recipe = (await Recipe.find(id)).rows[0]
-        if (!recipe) return res.redirect("/not-found")
-
-        return res.render("admin/recipes/recipe", {recipe} )
-    },
-    async edit (req, res) {
-        const { id } = req.params 
-        let recipe = (await Recipe.find(id)).rows[0] 
-        if (!recipe) return res.redirect("/not-found")
-
-        let chefOptions = (await Recipe.chefSelectOptions()).rows
-
-        return res.render("admin/recipes/edit", {recipe, chefOptions } )
-    },
-    async post (req, res) {
-        await Recipe.create(req.body)
-        return res.redirect("/admin/recipes")
-    }, 
-    async put (req, res) {
-        await Recipe.update(req.body)
-        return res.redirect(`/admin/recipes/${req.body.id}`)
-    }, 
-    async delete (req, res) {
-        await Recipe.delete(req.body.id)
-        return res.redirect("/admin/recipes/")
+  async index(req, res) {
+    try {
+      const recipes = (await RecipeModel.recipeInformations()).rows;
+      return res.render('admin/recipes/recipes', { recipes });
+    } catch (e) {
+      return res.status(500).send(e.message);
     }
-}
+  },
+  async create(req, res) {
+    try {
+      const chefOptions = (await RecipeModel.chefSelectOptions()).rows;
+      return res.render('admin/recipes/create', { chefOptions });
+    } catch (e) {
+      return res.status(500).send(e.message);
+    }
+  },
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const recipe = (await RecipeModel.find(id)).rows[0];
+      if (!recipe) return res.redirect('/not-found');
+
+      return res.render('admin/recipes/recipe', { recipe });
+    } catch (e) {
+      return res.status(500).send(e.message);
+    }
+  },
+  async edit(req, res) {
+    try {
+      const { id } = req.params;
+      const recipe = (await RecipeModel.find(id)).rows[0];
+      if (!recipe) return res.redirect('/not-found');
+
+      const chefOptions = (await RecipeModel.chefSelectOptions()).rows;
+
+      return res.render('admin/recipes/edit', { recipe, chefOptions });
+    } catch (e) {
+      return res.status(500).send(e.message);
+    }
+  },
+  async post(req, res) {
+    try {
+      await RecipeModel.create(req.body);
+      return res.redirect('/admin/recipes');
+    } catch (e) {
+      return res.status(500).send(e.message);
+    }
+  },
+  async put(req, res) {
+    try {
+      await RecipeModel.update(req.body);
+      return res.redirect(`/admin/recipes/${req.body.id}`);
+    } catch (e) {
+      return res.status(500).send(e.message);
+    }
+  },
+  async delete(req, res) {
+    try {
+      await RecipeModel.delete(req.body.id);
+      return res.redirect('/admin/recipes/');
+    } catch (e) {
+      return res.status(500).send(e.message);
+    }
+  },
+};
--- src/app/models/chef.js
+++ src/app/models/chef.js
@@ -1,58 +1,56 @@
-const database = require('../../config/db')
-const { date } = require('../../lib/utils')
+const Database = require('../../config/db');
+const { date } = require('../../lib/utils');
 
 module.exports = {
-    all() {
-        return database.query(`
+  all() {
+    return Database.query(`
             SELECT chefs.*, count(recipes.id) AS total_recipes
             FROM chefs
             LEFT JOIN recipes ON (chefs.id = recipes.chef_id)
-            GROUP BY chefs.id`)
-    },
-    create (data) {
-
-        const query = `
+            GROUP BY chefs.id`);
+  },
+  create(data) {
+    const query = `
             INSERT INTO chefs (
                 chef_name,
                 avatar_url,
                 created_at
             ) VALUES ($1, $2, $3)
             RETURNING id
-        `
-        const values = [
-            data.chef_name,
-            data.avatar_url,
-            date(Date.now()).format
-        ]
+        `;
+    const values = [
+      data.chef_name,
+      data.avatar_url,
+      date(Date.now()).format,
+    ];
 
-        return database.query(query, values)
-    },
-    find (id) {
-        return database.query(` 
+    return Database.query(query, values);
+  },
+  find(id) {
+    return Database.query(` 
             SELECT chefs.*, count(recipes.id) AS total_recipes
             FROM chefs
             LEFT JOIN recipes ON (recipes.chef_id = chefs.id)
             WHERE chefs.id = $1
-            GROUP BY chefs.id`, [id])
-    },
-    update (data) {
-        
-        const query = `
+            GROUP BY chefs.id`, [id]);
+  },
+  update(data) {
+    const query = `
             UPDATE chefs SET
                 chef_name = ($1),
                 avatar_url = ($2)
             WHERE id = $3
-        `
-        
-        const values = [
-            data.chef_name,
-            data.avatar_url,
-            data.id
-        ]
+        `;
 
-        return database.query(query, values)
-    },
-    delete (id) {
-        return database.query(` DELETE FROM chefs WHERE id = $1 `, [id])
-    }
-}
+    const values = [
+      data.chef_name,
+      data.avatar_url,
+      data.id,
+    ];
+
+    return Database.query(query, values);
+  },
+  delete(id) {
+    return Database.query(' DELETE FROM chefs WHERE id = $1 ', [id]);
+  },
+};
